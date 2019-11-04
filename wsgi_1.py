fc02028f6510015d492c789c55304e7fd7e58633
Dennis Lee <v-dele@microsoft.com>
2019-11-03 23:27:00 -0800

    Implemented the experimental WSGI server.

--- wsgi_1.py
+++ wsgi_1.py
@@ -19,13 +19,15 @@ def application(environ, start_response):
     import pprint
     pprint.pprint(environ)
 
+    this_day = datetime.datetime.today()
+
     response_body = body.format(
         software=environ.get('SERVER_SOFTWARE', default),
-        path="aaaa",
-        month="bbbb",
-        date="cccc",
-        year="dddd",
-        client_ip="eeee"
+        path=environ.get('PATH_INFO', default),
+        month=this_day.strftime("%B"),
+        date=this_day.day,
+        year=this_day.year,
+        client_ip=environ.get('REMOTE_ADDR')
     )
     status = '200 OK'
 
